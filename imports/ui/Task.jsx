835f26a920c0e97ff391bde96916e9304be1e749
tjval <gatrie@Didi.localdomain>
2017-05-07 20:51:34 -0400

    Added checkboxes to each task and ability to delete them

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,10 +1,43 @@
 import React, { Component, PropTypes } from 'react';
 
+import { Tasks } from '../api/tasks.js';
+
 // Task component - represents a single todo item
 export default class Task extends Component {
+  toggleChecked() {
+    // Set the checked property to opposite value
+    Tasks.update(this.props.tasks._id, {
+        $set: { checked: !this.props.task.checked },
+    });
+  }
+
+  // Deletes a task from the list and DB
+  deleteThisTask(){
+    Tasks.remove(this.props.task._id);
+  }
+
   render() {
+    // Give tasks a different className when they are checked off,
+    // so that we can style them differently
+    const taskClassName = this.props.task.checked ? 'checked' : '';
+
     return (
-      <li>{ this.props.task.text } </li>
+      <li className={taskClassName}>
+        <button className="delete" onClick={this.deleteThisTask.bind(this)}>
+        &times;
+        </button>
+
+        <input
+          type="checkbox"
+          readOnly
+          checked={this.props.task.checked}
+          onClick={this.toggleChecked.bind(this)}
+        />
+
+        <span className="text">
+          {this.props.task.text}
+        </span>
+      </li>
     );
   }
 }
